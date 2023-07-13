87f3a6dcd327a2701a5f0ef97822dfa991e4d2c3
SebastianKurkowski1 <sebastiankurkowski1013@gmail.com>
2023-07-14 01:16:07 +0200

    working with documentation, station routing

---
+++ src/Controller/BlogApiController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class BlogApiController extends AbstractController
+{
+    public function show(int $id): Response
+    {
+        // ... return a JSON response with the post
+    }
+
+    public function edit(int $id): Response
+    {
+        // ... edit a post
+    }
+}
