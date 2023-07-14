5bcb9ed44a144de3567d07aa7880796ee877ecec
SebastianKurkowski1 <sebastiankurkowski1013@gmail.com>
2023-07-14 22:46:06 +0200

    learning symfony

--- src/Controller/BlogApiController.php
+++ src/Controller/BlogApiController.php
@@ -4,10 +4,12 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
+
 class BlogApiController extends AbstractController
 {
     public function show(int $id): Response
     {
+      return new Response($id);
         // ... return a JSON response with the post
     }
 
--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,13 +3,18 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
 {
-    #[Route('/blog', name: 'blog_list')]
-    public function list(): Response
+    public function list(int $page): Response
     {
-// ...
+        return new Response($page);
+    }
+
+    public function show(BlogPost $post): Response
+    {
+        return new Response();
     }
 }
